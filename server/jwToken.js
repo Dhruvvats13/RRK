d87f671ae3fdd4b62b2de5b4f31a0fa9c657fb82
Dhruvvats13 <78378557+Dhruvvats13@users.noreply.github.com>
2023-05-02 21:06:40 +0530

    Update jwToken.js

--- server/jwToken.js
+++ server/jwToken.js
@@ -1,10 +1,20 @@
-const userSchema=require('./models/users');
-const jwt=require('jsonwebtoken')
-const  generateAuthToken=function(data){
-    data=JSON.stringify(data)
-    const token=jwt.sign(data,"ERUHIERHIDNFKLFUHIKSFNIFEIFIKFNHFFHSEIGHIIHF",{
-        // expiresIn:"30d",
-    });
-    return token
+const jwt = require('jsonwebtoken');
+const jwtSecret = "HaHa"
+const generateAuthToken= (req,res,next)=>{
+    // get the user from the jwt token and add id to req object
+    const token = req.header('auth-token');
+    if(!token){
+        res.status(401).send({error:"Invalid Auth Token"})
+
+    }
+    try {
+        const data = jwt.verify(token,jwtSecret);
+        req.user = data.user
+        next();
+        
+    } catch (error) {
+        res.status(401).send({error:"Invalid Auth Token"})
+    }
+
 }
-module.exports=generateAuthToken;
+module.exports = generateAuthToken
